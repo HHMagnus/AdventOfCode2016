4842fd9b373b6068cedb516636aaef224c50fb71
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-07-08 08:27:29 +0200

    day 5 part 2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -15,4 +15,24 @@ fn main() {
 	}
 	
 	println!("Day 5 part 1: {}", part1);
+
+	let mut part2 = vec!['-', '-', '-', '-', '-', '-', '-', '-'];
+	let mut hits = 0;
+	
+	let mut i = 0;
+	while hits < 8 {
+		let x = md5::compute(format!("{}{}", file, i));
+		if x.0[0] == 0 && x.0[1] == 0 && x.0[2] < 16 {
+			let pos = x.0[2] as usize;
+			let val = format!("{:x}", x.0[3] >> 4);
+			if pos < 8 && part2[pos] == '-' {
+				part2[pos] = val.chars().next().unwrap();
+				hits += 1;
+			}
+		}
+		i += 1;
+	}
+
+	let part2 = part2.into_iter().collect::<String>();
+	println!("Day 5 part 2: {}", part2);
 }
