4d6192ecdce37c7681509ed976ec5379323b4685
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-07-08 19:08:21 +0200

    day 9 part 2

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -34,4 +34,41 @@ fn main() {
 	}
 
 	println!("Day 9 part 1: {}", length);
+	let part2 = part2(&file);
+	println!("Day 9 part 2: {}", part2);
+}
+
+fn part2(file: &str) -> usize {
+	let mut it = file.chars();
+	let mut length = 0;
+
+	while let Some(next) = it.next() {
+		if next == '(' {
+			let mut start = String::new();
+			while let Some(x) = it.next() {
+				if x == 'x' {
+					break;
+				}
+				start.push(x);
+			}
+			let mut end = String::new();
+			while let Some(x) = it.next() {
+				if x == ')' {
+					break;
+				}
+				end.push(x);
+			}
+			let start = start.parse::<usize>().unwrap();
+			let end = end.parse::<usize>().unwrap();
+			let mut compressed = String::new();
+			for _ in 0..start {
+				compressed.push(it.next().unwrap());
+			}
+			length += part2(&compressed) * end;
+			continue;
+		}
+		length += 1;
+	}
+
+	return length;
 }
