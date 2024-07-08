8e510ab81733db2cd8ba95c687c5f2e34ebfb67d
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-07-08 09:37:50 +0200

    day 7 part 2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,10 +1,13 @@
-use std::fs::read_to_string;
+use std::{collections::HashSet, fs::read_to_string};
 
 fn main() {
 	let file = read_to_string("input/day7.txt").unwrap();
 
 	let part1 = file.lines().into_iter().filter(|line| support_tls(line)).count();
 	println!("Day 7 part 1: {}", part1);
+
+	let part2 = file.lines().into_iter().filter(|line| support_ssl(line)).count();
+	println!("Day 7 part 2: {}", part2);
 }
 
 fn support_tls(line: &str) -> bool {
@@ -48,4 +51,45 @@ fn support_tls(line: &str) -> bool {
 	}
 	
 	result
+}
+
+
+
+fn support_ssl(line: &str) -> bool {
+	let mut inside = false;
+	
+	let mut it1 = line.chars();
+	let mut it2 = line.chars();
+	it2.next();
+	let mut it3 = line.chars();
+	it3.next();
+	it3.next();
+
+	let mut insides = HashSet::new();
+	let mut outsides = HashSet::new();
+
+	while let Some(c3) = it3.next() {
+		if let Some(c1) = it1.next() {
+			if let Some(c2) = it2.next() {
+				if c1 == '[' || c2 == '[' || c3 == '[' {
+					inside = true;
+					continue;
+				}
+				if c1 == ']' || c2 == ']' || c3 == ']' {
+					inside = false;
+					continue;
+				}
+
+				if c1 == c3 {
+					if inside {
+						insides.insert((c1, c2, c3));
+					} else {
+						outsides.insert((c1, c2, c3));
+					}
+				}
+			}
+		}
+	}
+
+	insides.iter().any(|(c1, c2, _)| outsides.iter().any(|(x1, x2, x3)| x2 == c1 && x1 == c2 && x3 == c2))
 }
