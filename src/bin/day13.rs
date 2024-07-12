9c798201cd20da735178c83a88ef6345687a3c24
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-07-12 14:00:17 +0200

    day 13 part 1

---
+++ src/bin/day13.rs
@@ -0,0 +1,50 @@
+use std::{collections::{HashSet, VecDeque}, fs::read_to_string};
+
+fn main() {
+    let file = read_to_string("input/day13.txt").unwrap();
+
+	let input = file.parse::<usize>().unwrap();
+
+	let start = (1, 1);
+
+	let mut queue = VecDeque::new();
+	let mut visited = HashSet::new();
+	visited.insert(start);
+	queue.push_back((0, (0,0)));
+
+	let end = (31, 39);
+
+	while let Some((steps, (x, y))) = queue.pop_front() {
+		if (x,y) == end {
+			println!("Day 13 part 1: {}", steps);
+			break;
+		}
+
+		let neighbours = [
+			(x - 1, y),
+			(x + 1, y),
+			(x, y - 1),
+			(x, y + 1),
+		].into_iter().filter(|&(x, y)| x >= 0 && y >= 0).filter(|&(x, y)| open(input, x, y));
+
+		for neigh in neighbours {
+			if visited.contains(&neigh) {
+				continue;
+			}
+			visited.insert(neigh);
+			queue.push_back((steps+1, neigh));
+		}
+	}
+}
+
+fn open(input: usize, x: i32, y: i32) -> bool {
+	let mut v = x*x + 3*x + 2*x*y + y + y*y + input as i32;
+	let mut count = 0;
+	while v > 0 {
+		if v & 1 > 0 {
+			count += 1;
+		}
+		v >>= 1;
+	}
+	count % 2 == 0
+}
