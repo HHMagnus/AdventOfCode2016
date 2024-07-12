ae2a3301fb4421833d97966f2cca2aaa612473f8
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-07-12 14:50:02 +0200

    day 14 part 2

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -3,7 +3,7 @@ use std::fs::read_to_string;
 fn main() {
     let file = read_to_string("input/day14.txt").unwrap();
 
-	let hashes = (0..100000).map(|x| md5::compute(format!("{}{}", file, x))).map(|x| format!("{:?}", x)).map(|x| x.chars().into_iter().collect::<Vec<_>>()).collect::<Vec<_>>();
+	let hashes = (0..50000).map(|x| md5::compute(format!("{}{}", file, x))).map(|x| format!("{:?}", x)).map(|x| x.chars().into_iter().collect::<Vec<_>>()).collect::<Vec<_>>();
 
 	let mut fives = Vec::new();
 
@@ -29,5 +29,43 @@ fn main() {
 				break;
 			}
 		}
+	}
+
+	let mut i = -1000;
+	let mut key = 0;
+	let mut fives = Vec::new();
+	let mut hashes = Vec::new();
+
+	while key < 64 {
+		let mut hash = format!("{}{}", file, i+1000);
+
+		for _ in 0..2017 {
+			hash = format!("{:?}", md5::compute(hash));
+		}
+		let hash = hash.chars().into_iter().collect::<Vec<_>>();
+		hashes.push(hash.clone());
+		
+		for window in hash.windows(5) {
+			if window.iter().all(|&w| w == window[0]) {
+				fives.push((i+1000, window[0]));
+			}
+		}
+
+		if i >= 0 {
+			let hash = &hashes[i as usize];
+			for window in hash.windows(3) {
+				if window[0] == window[1] && window[1] == window[2] {
+					if fives.iter().any(|&(i0, x0)| i < i0 && i0 < i+1000 && x0 == window[0]) {
+						key += 1;
+						if key == 64 {
+							println!("Day 14 part 2: {}", i);
+						}
+					}
+					break;
+				}
+			}
+		}
+
+		i += 1;
 	}
 }
