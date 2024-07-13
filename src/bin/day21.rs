e1de171ce4fca9d37bc781562d32a0692b83bd20
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-07-13 11:11:56 +0200

    day 21 part 2

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{collections::VecDeque, fs::read_to_string};
 
 #[derive(Debug)]
 enum Instruction {
@@ -49,8 +49,60 @@ fn main() {
 		}
 	}).collect::<Vec<_>>();
 
-	println!("{:?}", instructions);
+	let mut input = "abcdefgh".chars().collect::<VecDeque<_>>();
 
-	let input = "abcdefgh";
-	
+	for inst in instructions {
+		match inst {
+			Instruction::SwapPostion(x, y) => {
+				let c1 = input[x];
+				let c2 = input[y];
+				input[x] = c2;
+				input[y] = c1;
+			},
+			Instruction::SwapLetter(x, y) => {
+				let i = input.iter().enumerate().find(|&(_, &c)| c == x).unwrap().0;
+				let j = input.iter().enumerate().find(|&(_, &c)| c == y).unwrap().0;
+				input[i] = y;
+				input[j] = x;
+			},
+			Instruction::RotateLeft(r) => {
+				for _ in 0..r {
+					let c = input.pop_front().unwrap();
+					input.push_back(c);
+				}
+			},
+			Instruction::RotateRight(r) => {
+				for _ in 0..r {
+					let c = input.pop_back().unwrap();
+					input.push_front(c);
+				}
+			},
+			Instruction::RotateLetter(x) => {
+				let i = input.iter().enumerate().find(|&(_, &c)| c == x).unwrap().0;
+				let mut total = i + 1;
+				if i >= 4 {
+					total += 1;
+				}
+				for _ in 0..total {
+					let c = input.pop_back().unwrap();
+					input.push_front(c);
+				}
+			},
+			Instruction::Reverse(x, y) => {
+				let mut new = Vec::new();
+				for i in x..=y {
+					new.push(input[i]);
+				}
+				for i in x..=y {
+					input[i] = new.pop().unwrap();
+				}
+			},
+			Instruction::Move(x, y) => {
+				let c = input.remove(x).unwrap();
+				input.insert(y, c);
+			},
+		}
+	}
+
+	println!("Day 21 part 1: {}", input.into_iter().collect::<String>());
 }
