1d06b634eb987b677306fd9d8beaffa9628e45ba
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-07-13 09:56:40 +0200

    day 18 part 1

---
+++ src/bin/day18.rs
@@ -0,0 +1,26 @@
+use std::{collections::VecDeque, fs::read_to_string};
+
+fn main() {
+    let file = read_to_string("input/day18.txt").unwrap();
+	let mut rows = Vec::new();
+	rows.push(file.chars().collect::<VecDeque<_>>());
+	for i in 1..40 {
+		let mut prev_row = rows[i-1].clone();
+		prev_row.push_front('.');
+		prev_row.push_back('.');
+
+		let row = prev_row.into_iter().collect::<Vec<_>>();
+		let row = row.into_iter().as_slice().windows(3).map(|x| if is_trap(x) { '^' } else { '.' }).collect::<VecDeque<_>>();
+		rows.push(row);
+	}
+
+	let part1 = rows.into_iter().flat_map(|x| x).filter(|&x| x == '.').count();
+	println!("Day 18 part 1: {}", part1);
+}
+
+fn is_trap(x: &[char]) -> bool {
+	(x[0] == '^' && x[1] == '^' && x[2] == '.')
+	|| (x[0] == '.' && x[1] == '^' && x[2] == '^')
+	|| (x[0] == '.' && x[1] == '.' && x[2] == '^')
+	|| (x[0] == '^' && x[1] == '.' && x[2] == '.')
+}
